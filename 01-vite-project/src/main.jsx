81b25b23a662cd5bd2f3743ec48a94353092c625
JayPatel1202 <jaykumarpatel1202@gmail.com>
2025-05-14 22:28:59 +0530

    04-Understanding-Rendering-JSX-to-React

--- 01-vite-project/src/main.jsx
+++ 01-vite-project/src/main.jsx
@@ -1,9 +1,26 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import React from "react";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// function SampleFun() {
+//   return <h1>Hello I am React.</h1>;
+// }
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// };
+
+// const reactElement = React.createElement(
+//   "a",
+//   { href: "https://google.com", target: "_blank" },
+//   "Click me to visit google"
+// );
+
+// const anotherElement = <a href="https://google.com">Visit Google</a>;
+
+createRoot(document.getElementById("root")).render(<App />);
