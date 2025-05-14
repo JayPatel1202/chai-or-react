7c04df55663880ac3d0f0f649a70ae6272be3667
JayPatel1202 <jaykumarpatel1202@gmail.com>
2025-05-14 20:42:09 +0530

    02-Basic-React-Folder-Vite

---
+++ 01-basic-react/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chai-or-react</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-vite-project/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Chai-or-react-vite</h1>;
+}
+
+export default App;
